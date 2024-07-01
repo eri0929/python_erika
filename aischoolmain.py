441348bc94dce99430a69a155a9f8c3a1ca6b203
eri0929 <l1q1d@naver.com>
2024-07-01 17:02:45 +0900

    Update aischoolmain.py

--- aischoolmain.py
+++ aischoolmain.py
@@ -9,15 +9,14 @@ user_password = st.sidebar.text_input('패스워드 입력', value='1234', type=
 
 if user_password =='1234':
     st.sidebar.header('Erika World')
-    opt_data = ['환율조회','따릉이','유성우']
+    opt_data = ['   ' , '환율조회','따릉이','유성우']
     menu = st.sidebar.selectbox('메뉴 선택', opt_data, index=0)
     st.sidebar.write('선택한 메뉴', menu) 
 
     if menu =='환율조회':
         st.subheader('환율조회>>>>>>>>>>')
         finance_naver0701.exchange_main()
-        
-
+      
     elif menu=='따릉이':
         st.subheader('따릉이 데이터 분석>>>>>>>>>>')
     
